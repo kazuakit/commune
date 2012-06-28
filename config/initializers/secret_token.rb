9f61fce8406e8b17d24e3054159b27b72984a789
伊藤和明 <ditkito@mediba.jp>
2012-06-28 11:01:03 +0900

    rails new commune -T -d mysql --skip-bundle; cd commune; bundle install --path=vendor/bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Commune::Application.config.secret_token = 'b88f7653f990439eade711bc13d462eb0443b381683e94cf0ffcf8e37bbfcebecc123593597c002a51576c1582de13079015796b9bf34f3005b24ff658dba53c'
